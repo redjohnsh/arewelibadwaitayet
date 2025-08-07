eb1707cc2d23ab42da9b3efb74d6ebbc63b1b497
Gabriel Vaquer <brielov@icloud.com>
2025-08-07 10:20:19 -0300

    Make plausible environment variable static instead of dynamic

--- src/lib/plausible.ts
+++ src/lib/plausible.ts
@@ -1,8 +1,8 @@
-import { env } from '$env/dynamic/public';
+import { PUBLIC_PLAUSIBLE_API_HOST } from '$env/static/public';
 import { Plausible } from 'plausible-client';
 
 export const plausible = new Plausible({
-	apiHost: env.PUBLIC_PLAUSIBLE_API_HOST,
+	apiHost: PUBLIC_PLAUSIBLE_API_HOST,
 	domain: 'arewelibadwaitayet.com',
 	filter(event) {
 		if (event.url.includes('localhost')) {
