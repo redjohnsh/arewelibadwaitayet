64f71b98c888e143c224c6c0d54bbe43d5a021e5
Gabriel Vaquer <brielov@icloud.com>
2025-08-07 10:13:36 -0300

    Make plausible host an environment variable

--- src/lib/plausible.ts
+++ src/lib/plausible.ts
@@ -1,7 +1,8 @@
+import { env } from '$env/dynamic/public';
 import { Plausible } from 'plausible-client';
 
 export const plausible = new Plausible({
-	apiHost: 'https://analytics.arewelibadwaitayet.com',
+	apiHost: env.PUBLIC_PLAUSIBLE_API_HOST,
 	domain: 'arewelibadwaitayet.com',
 	filter(event) {
 		if (event.url.includes('localhost')) {
