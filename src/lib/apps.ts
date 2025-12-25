6033faf2cf75b11ad0054718e50c7eda627b16fa
Benedek Dévényi <rdbende@proton.me>
2025-12-25 12:05:57 +0100

    Added TV Remote

--- src/lib/apps.ts
+++ src/lib/apps.ts
@@ -516,7 +516,8 @@ export const APP_LANGUAGES: AppLanguageMap = {
 	'io.github.shonebinu.Brief': Lang.Python,
 	'io.github.diegopvlk.Tomatillo': Lang.Python,
 	'one.naiara.Kuychi': Lang.TypeScript,
-	'io.github.falldaemon.engplayer': Lang.Python
+	'io.github.falldaemon.engplayer': Lang.Python,
+	'io.github.erenseymen.android-tv-remote': Lang.Python
 };
 
 // Client-side prepared app type (for reference, but not used in server code)
