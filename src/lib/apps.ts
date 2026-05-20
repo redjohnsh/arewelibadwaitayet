6255ba9a62e30486854148a574184e16aa6df7d6
Solivan <qualitychronology@gmail.com>
2026-05-19 19:48:52 -0600

    Added Press App

--- src/lib/apps.ts
+++ src/lib/apps.ts
@@ -577,6 +577,7 @@ export const APP_LANGUAGES: AppLanguageMap = {
 	'ca.vtrlx.Parchment': Lang.Lua,
 	'xyz.riothedev.emojify': Lang.CPlusPlus,
 	'art.fatdawlf.Piccolo': Lang.Rust,
+	'io.github.masakk1.press' Lang.Vala,
 };
 
 // Client-side prepared app type (for reference, but not used in server code)
